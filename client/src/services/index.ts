aea9db2a92c98c804b73f05e6721688d846733c9
flavingimenes <gflavio581@gmail.com>
2026-05-20 14:46:52 -0400

    little fix

--- client/src/services/index.ts
+++ client/src/services/index.ts
@@ -3,5 +3,5 @@ export { default as api } from './api';
 export * from './auth/auth.service';
 export * from './auth/auth.types';
 
-export { default as missionService } from './domain/mission.service';
-export * from './domain/mission.service';
+export { default as userService } from './domain/user.service';
+export * from './domain/user.service';
